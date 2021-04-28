3778c9bcfa0183336e074329373e3698c633dbcb
XeGrox <xegrox@protonmail.com>
2021-04-28 20:58:30 +0800

    test(reference): add tests for validating reference name

---
+++ tests/reference_test.dart
@@ -0,0 +1,86 @@
+import 'package:test/test.dart';
+
+import 'package:dart_git/src/exceptions.dart';
+import 'package:dart_git/src/plumbing/reference.dart';
+
+void main() {
+  group('Test reference name', () {
+    test('When_NameStartWithDot_Should_ThrowException', () {
+      var refName = '.dummy';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+    });
+
+    test('When_NameStartsWithSlash_Should_ThrowException', () {
+      var refName = '/dummy';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+    });
+
+    test('When_NameEndsWithDot_Should_ThrowException', () {
+      var refName = 'dummy.';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+    });
+
+    test('When_NameEndsWithSlash_Should_ThrowException', () {
+      var refName = 'dummy/';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+    });
+
+    test('When_NameEndsWithDotLock_Should_ThrowException', () {
+      var refName = 'dummy.lock';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+    });
+
+    test('When_NameIs@_Should_ThrowException', () {
+      var refName = '@';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+    });
+
+    test('When_NameHasInvalidChar_Should_ThrowException', () {
+      // '@{' sequence
+      var refName = 'd@{d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // two consecutive dots '..'
+      refName = 'd..d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // multiple consecutive slashes
+      refName = 'd//d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // ascii control chars
+      refName = 'd\x00d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      refName = 'd\x19d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      refName = 'd\x7Fd';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // space
+      refName = 'd d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // tilde
+      refName = 'd~d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // caret
+      refName = 'd^d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // colon
+      refName = 'd:d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // question mark
+      refName = 'd?d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // asterisk
+      refName = 'd*d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // open square bracket
+      refName = 'd[d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+      // backslash
+      refName = r'd\d';
+      expect(() => GitReferenceHash(refName, null), throwsA(TypeMatcher<InvalidReferenceNameException>()));
+    });
+
+    test('When_NameIsValid_ShouldNotThrowException', () {
+      var refName = 'd.lock@mmy/du]m/Y\x80';
+      GitReferenceHash(refName, null);
+    });
+  });
+}
